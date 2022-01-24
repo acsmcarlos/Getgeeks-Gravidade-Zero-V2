98790b449d01da977ea65965f74cfc543175a104
ANTONIO CARLOS <acsmcarlos@gmail.com>
2022-01-24 19:19:05 -0300

    Ajustes nos testes

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,16 +1,9 @@
-# clear
-# rm -rf logs/
-
-# EXECUTA O DELOREAN SEM GERAR O LOG (NONE) ============|
-robot -r NONE -l NONE -o NONE Tasks/Delorean.robot
-
-## TRATAMENTO DAS SCREENSHOTS DO PABOT
+robot -l NONE -o NONE -r NONE tasks/Delorean.robot
 pabot -x xunit.xml -d ./logs -i smoke Testes/
 
 rm -rf ./logs/browser
 mkdir ./logs/browser
 mkdir ./logs/browser/screenshot
 
-cp $(find ./logs/pabot_results -type f -name "*.png") ./logs/browser/screenshot
-# rm -rf logs/pabot_results/
+cp $(find ./logs/pabot_results -type f -name "*.png") ./logs/browser/screenshot/
 
