b1dacafcba2faf0fea80a0d7023a8babeb85563d
ANTONIO CARLOS <acsmcarlos@gmail.com>
2022-01-24 17:51:39 -0300

    Iniciando o Job de testes para web

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,25 +1,16 @@
-clear
-rm -rf Logs/
+# clear
+# rm -rf logs/
 
 # EXECUTA O DELOREAN SEM GERAR O LOG (NONE) ============|
 robot -r NONE -l NONE -o NONE Tasks/Delorean.robot
 
-# TODOS OS TESTES ======================================|
-# robot -d ./Logs Testes/
-# robot -d ./Logs Testes/AttemptBeGeek.robot
-# robot -d ./Logs Testes/BeGeek.robot
-# robot -d ./Logs Testes/Login.robot
-# robot -d ./Logs Testes/Signup.robot
-
-# TESTE DE FUMAÇA (PRINCIPAIS) ==========================|
-# robot -d ./Logs -i smoke Testes/
-
-
 ## TRATAMENTO DAS SCREENSHOTS DO PABOT
-robot -d ./Logs Testes/
-rm -rf Logs/browser
-mkdir -p ./Logs/browser
-mkdir ./Logs/browser/screenshot
-cp $(find ./Logs/pabot_results -type f -name "*.png") ./Logs/browser/screenshot
-rm -rf Logs/pabot_results/
+pabot -x xunit.xml -d ./logs -i smoke Testes/
+
+rm -rf ./logs/browser
+mkdir ./logs/browser
+mkdir ./logs/browser/screenshot
+
+cp $(find ./logs/pabot_results -type f -name "*.png") ./logs/browser/screenshot
+# rm -rf logs/pabot_results/
 
