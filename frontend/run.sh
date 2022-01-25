362b4febbbe683c2b3d23e703d93fad1f12a04db
ANTONIO CARLOS <acsmcarlos@gmail.com>
2022-01-25 10:28:21 -0300

    Remoção do pabot

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,9 +1,10 @@
 robot -l NONE -o NONE -r NONE tasks/Delorean.robot
-pabot -x xunit.xml -d ./logs -i smoke Testes/
 
-rm -rf ./logs/browser
-mkdir ./logs/browser
-mkdir ./logs/browser/screenshot
+robot -x xunit.xml -d ./logs -i smoke Testes/
 
-cp $(find ./logs/pabot_results -type f -name "*.png") ./logs/browser/screenshot/
+# rm -rf ./logs/browser
+# mkdir ./logs/browser
+# mkdir ./logs/browser/screenshot
+
+# cp $(find ./logs/pabot_results -type f -name "*.png") ./logs/browser/screenshot/
 
